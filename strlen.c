05ac7caeb48e1af901d464c948a897799a8f602c
tkddnr3341 <tkddnr3341@naver.com>
2021-03-06 17:04:03 +0900

    lengthstr

---
+++ strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{	
+	char str[]="James Harden is the best offensive player, in basketball, the world has ever seen.";
+	int i=0;
+	while (str[i]!='\0')
+	{
+		i++;
+	}
+	printf("length of string: %d\n",i);
+	return 0;
+}
+       	
