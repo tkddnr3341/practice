8febfcf903aaf430505cb9470b64edce458ee5b9
Guest <guest-vmaaje@h20077.sgb.skir.kr>
2021-03-06 16:37:21 +0900

    type of str

---
+++ print_str.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	char str1[10]="lebron";
+	char str2[10]={'h','a','r','d','e','n'};
+	char str3[]="who is the best player among these two?";
+	printf("%s \n%s \n%s \n",str1,str2,str3);
+	return 0;
+}
