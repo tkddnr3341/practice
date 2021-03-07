3222ae01702091e316a1269cdea17d6b3df65cd7
Guest <guest-ncqsx0@h20077.sgb.skir.kr>
2021-03-07 10:56:02 +0900

    change string

--- changestr.c
+++ changestr.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
-void change_str(char string[10]);
+
+void change_str(char string[],int len1);
+int lenstr(char string[]);
 
 int main()
 {
-	char str[10]="Lebron";
-	change_str(str);	
+	char str[]="King Lebron James ";
+	int len;
+	len=lenstr(str);
+	change_str(str,len);	
 	printf("%s\n",str);
 	return 0;
 }
 
-void change_str(char string[10])
+void change_str(char string[],int len1)
 {
-	char str[10]="Harden";
-	for (int i=0;i<10;i++)
+	char str[]="Jame Harden";
+	int len2;
+	len2=lenstr(str);
+	if(len1>=len2)
 	{
-		string[i]=str[i];
+       		for (int i=0;i<len1;i++)
+		{
+			if (i<len2)
+			{
+				string[i]=str[i];
+			}
+			else 
+			{
+				string[i]='\0';
+			}
+		}
+       	printf("%s\n",string);
 	}
-	printf("%s\n",string);
+	else
+	{
+		printf("too long\n");
+	}
+}
+
+int lenstr(char string[])
+{
+	int size=0;
+	while (string[size]!='\0')
+	{
+		size+=1;
+	}
+	return size;
 }
