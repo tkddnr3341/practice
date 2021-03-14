e8503def295c561aa0d2bb9f44fb7920061779ab
tkddnr3341 <tkddnr3341@naver.com>
2021-03-14 13:38:16 +0900

    2Darray & pointer array

--- 2_dim_array.c
+++ 2_dim_array.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 void print_strings(char darray[][50],int size);
-void print_pointerarray(char *array[],int size);
+void print_pa(char *array[],int size);
 
 int main()
 {
 	char string[3][50]={"James Harden",
 		"Kevin Durant",
 		"Kyrie Irving"};
-	char *pointerarray[3];
-	pointerarray=&string;
+	char *pointerarray[3]={"SSS class",
+		"suicidal",
+		"Hunter"};
 	print_strings(string,3);
-	print_pointerarray(pointerarray,3);
+	print_pa(pointerarray,3);
 	return 0;
 }
 
@@ -22,16 +23,14 @@ void print_strings(char darray[][50],int size)
 		p=darray[i1];
 		printf("%s\n",p);
 	}
-	printf("\n");
 }
 
-void print_pointer(char *array[],int size)
+void print_pa(char *array[],int size)
 {
 	for (int i=0;i<size;i++)
 	{
 		printf("%s\n",array[i]);
 	}
-	printf("\n");
 }
 
 
