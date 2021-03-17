733996b035972aae866a729f14e8bc364078bbab
Guest <guest-c99sku@h20077.sgb.skir.kr>
2021-03-17 16:43:13 +0900

    print_string

--- print_str.c
+++ print_str.c
@@ -4,6 +4,6 @@ int main()
 	char str1[10]="lebron";
 	char str2[10]={'h','a','r','d','e','n'};
 	char str3[]="who is the best player among these two?";
-	printf("%s \n%s \n%s \n",str1,str2,str3);
+	printf("%s \n%s \n%s \n",str1,str2,str3);		//printf는 문자열을 받는 것이 아닌 문자열의 주소를 받는다.
 	return 0;
 }
