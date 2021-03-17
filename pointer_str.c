5c41e1fd4d23e0e55396daa139518bda12ebeffc
Guest <guest-c99sku@h20077.sgb.skir.kr>
2021-03-17 17:06:03 +0900

    pointer string

--- pointer_str.c
+++ pointer_str.c
@@ -2,10 +2,16 @@
 
 int main()
 {
+	int answer;
 	char *p="Lebron is the best";
+	char *q="James Harden is the best";
+	p=q;
 	printf("%s\n",p);
-	p="James Harden is the best";
-	printf("%s\n",p);
+	if (p==q)
+	{
+		answer=0;
+	}
+	printf("%d\n",answer);
 	/*
 	p[0]='A';
 	p: data segment(changeable)    "James Harden is the best": text segment(unchangable)
